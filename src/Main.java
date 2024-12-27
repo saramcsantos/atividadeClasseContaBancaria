fbf071982ea7de50e02d975ccea9dcf2d4b96ae5
saramcsantos <contatosarams@gmail.com>
2024-12-27 12:42:50 -0300

    Uso de todos os métodos, criação de um novo objeto e uso do método cobraTarifaMensal

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import br.com.alura.contabancaria.model.ContaBancaria;
+import br.com.alura.contabancaria.model.ContaCorrente;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,14 +12,25 @@ public class Main {
         System.out.println(String.format("Conta: %d | Titular: %s | Saldo atual: % .2f",
                 conta.getMumeroDaConta(), conta.nomeDoTitular, conta.getSaldoDaConta()));
 
-        conta.recebeValor(150);
+        System.out.println("---------------------------------");
+        conta.depositaValor(200);
+        conta.consultaSaldo(); // 1200
 
-        System.out.println(String.format("Conta: %d | Titular: %s | Saldo atual: % .2f",
-                conta.getMumeroDaConta(), conta.nomeDoTitular, conta.getSaldoDaConta()));
+        System.out.println("---------------------------------");
+        conta.sacaValor(40);
+        conta.consultaSaldo(); // 1160
 
-        conta.transfereValor(200);
+        System.out.println("---------------------------------");
+        double valor = conta.transfereValor(40);
+        conta.consultaSaldo(); // 1120
 
-        System.out.println(String.format("Conta: %d | Titular: %s | Saldo atual: % .2f",
-                conta.getMumeroDaConta(), conta.nomeDoTitular, conta.getSaldoDaConta()));
+        System.out.println("---------------------------------");
+        conta.recebeValor(valor);
+        conta.consultaSaldo(); //1160
+
+        System.out.println("---------------------------------");
+        ContaCorrente contacorrente = new ContaCorrente();
+        contacorrente.setTarifaMensal(100);
+        contacorrente.cobraTarifaMensal(conta);
     }
 }
